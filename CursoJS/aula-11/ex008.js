2bfc0109dc922877ac54ff5b438c12aa550c20c7
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-02-05 21:50:06 -0300

    .

---
+++ CursoJS/aula-11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida e recebeu uma multa de $40,00.`)
+    console.log(`Dirija com atenção no limite máximo da velocidade permitida`)
+}
+console.log(`Siga em frente.`)
+
