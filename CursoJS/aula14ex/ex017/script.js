2b6ca8e8a9bc277708cf18c0cc3240f27d956f80
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-02-22 23:14:34 -0300

    .

--- CursoJS/aula14ex/ex017/script.js
+++ CursoJS/aula14ex/ex017/script.js
@@ -1,5 +1,21 @@
 function contar() {
-    let ini = t
-    let fim = t
-    let passo = t
+    let ini = document.getElementById ('txti')
+    let fim = document.getElementById ('txtf')
+    let passo = document.getElementById ('txtp')
+    let res = document.getElementById ('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
+    }
+
+
 }
