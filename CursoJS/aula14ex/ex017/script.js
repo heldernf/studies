bdb2118db1fdc8e4da3e1ca72289beee11bc2353
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-02-24 01:30:53 -0300

    .

--- CursoJS/aula14ex/ex017/script.js
+++ CursoJS/aula14ex/ex017/script.js
@@ -3,19 +3,33 @@ function contar() {
     let fim = document.getElementById ('txtf')
     let passo = document.getElementById ('txtp')
     let res = document.getElementById ('res')
+    let h3 = document.getElementById ('h3-res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados!')
+        h3.innerHTML = 'Impossivel contar, faltam dados! [ERRO 446]'
+        h3.style.color = 'red'
+        res.innerHTML = ''
     } else {
-        res.innerHTML = 'Contando...'
+        h3.innerHTML = 'resultado...'
+        h3.style.color = 'white'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
+        if (p <= 0) {
+            h3.innerHTML = 'Passo inválido, agora passo vale 1. Resultado:'
+            p = 1
         }
+        if (i < f) {
+            // CONTAGEM CRESCENTE
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}\u{1F449} `
+            }
+        } else {
+            // CONTAGEM REGRESSIVA
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c}\u{1F449} `               
+            }
+        }
+        res.innerHTML += `\u{1F6A9}FIM\u{1F6A9}`
     }
-
-
 }
--- CursoJS/aula14ex/modelo/script.js
+++ CursoJS/aula14ex/modelo/script.js
@@ -1,41 +1,18 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano) {
-        alert('[ERRO] Verifique os dados e tente novamente!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'img/h-bebe.png')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'img/h-jovem.png')
-            } else if (idade < 50) {
-                img.setAttribute('src', 'img/h-adulto.png')
-            } else {
-                img.setAttribute('src', 'img/h-velho.png')          
-            }
-        } else {
-            genero = 'Mulher'
-            if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'img/m-bebe.png')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'img/m-jovem.png')
-            } else if (idade < 50) {
-                img.setAttribute('src', 'img/m-adulto.png')
-            } else {
-                img.setAttribute('src', 'img/m-velha.png')          
-            }
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement ('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild (item)
+            c++
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
     }
 }
