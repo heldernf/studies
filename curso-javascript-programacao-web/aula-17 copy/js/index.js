f52c6fe27dde767ced4bf7ed08051b5d40e32318
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-05-30 19:35:56 -0300

    finalização projeto de "fabrica de carros"
    Correções.

--- curso-javascript-programacao-web/aula-17 copy/js/index.js
+++ curso-javascript-programacao-web/aula-17 copy/js/index.js
@@ -10,6 +10,8 @@ class Carro {
     }
 }
 
+var timeoutId;
+
 function fabCar() {
     var h1 = document.getElementById('h1-res');
     var h2 = document.getElementById('h2-res');
@@ -18,23 +20,44 @@ function fabCar() {
     var mo = document.getElementById('modelo').value;
     var a = document.getElementById('ano').value;
 
-    
-
-    if (m == "" || mo == "" || a == "") {
-        h1.innerHTML = "Preencha todos os campos."
+    if (m == "" || mo == "" || a == "" || a < 1950 || a > 2023) {
+        if (a < 1950 || a > 2023) {
+            h1.style.color = "red";
+            h1.innerHTML = "A ano do carro não pode ser menor que 1950 ou maior que 2023.";
         
-        h2.innerHTML = ""
+            h2.innerHTML = "";
 
-        setTimeout (function () {
-            h1.innerHTML = ""
-        }, 5000)
+            timeoutId = setTimeout (function () {
+                h1.innerHTML = "Tente novamente!";
+                
+                setTimeout (function () {
+                    h1.innerHTML = "";
+                }, 1000);
+
+            }, 3000)
+        } else {
+            h1.style.color = "red";
+            h1.innerHTML = "Preencha todos os campos!";
+            
+            h2.innerHTML = "";
+            
+            timeoutId = setTimeout (function () {
+                h1.innerHTML = "Tente novamente!";
+
+                setTimeout (function () {
+                    h1.innerHTML = "";
+                }, 1000)
+
+            }, 3000);
+        }
     } else {
+        clearTimeout(timeoutId);
+
         const carro = new Carro(m, mo, a);
-        console.log(carro)
+        
+        h1.style.color = "black";
+        h1.innerHTML = "Seu carro:";
 
-        h1.innerHTML = "Seu carro:"
-
-        h2.innerHTML = " " + carro.marca + " " + carro.modelo + " " + carro.ano
+        h2.innerHTML = "<span style='color: rgb(27, 98, 190);'>Marca:</span>" + carro.marca + " <span style='color: rgb(27, 98, 190);'>Modelo:</span>" + carro.modelo + " <span style='color: rgb(27, 98, 190);'>Ano:</span>" + carro.ano;
     }
-
 }
