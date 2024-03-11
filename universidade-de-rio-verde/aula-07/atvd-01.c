202afecae106ec2465b34aaa31b8e94845beb1ea
Helder Neto <helderneto2005@gmail.com>
2024-03-11 17:09:18 -0300

    Correção
    Correção do final do código, pois o notbook desligou e n deu tempo q de terminar.

--- universidade-de-rio-verde/aula-07/atvd-01.c
+++ universidade-de-rio-verde/aula-07/atvd-01.c
@@ -4,22 +4,24 @@
 int main () {
     setlocale(LC_ALL, "Portuguese_Brazil");
 
-    int eleitores, vBranco, vNulo, vValido, pBranco, pNulo;
+    int eleitores, vBranco, vNulo, vValido, pBranco, pNulo, pValido;
 
-    printf("Digite o total de eleitores:\n");
+    printf("\nDigite o total de eleitores:\n");
     scanf("%d", &eleitores);
-    printf("Agora digite e envie um por vez na seguinte oredem: Votos em Branco | Votos Nulos\n");
+    printf("\nAgora digite e envie um por vez na seguinte oredem: Votos em Branco | Votos Nulos\n");
     scanf("%d %d", &vBranco, &vNulo);
 
-    vValido = vBranco + vNulo - eleitores;
-    if (vValido - 0) {
-        vValido *= -1;
+    vValido = eleitores - vBranco - vNulo;
+
+    if (vValido >= 0) {
+        pValido = (vValido * 100) / eleitores;
+        pBranco = (vBranco * 100) / eleitores;
+        pNulo =  (vNulo * 100) / eleitores;
+
+        printf("\nTotal de votos - %d\nVotos válidos - %d%%\nVotos brancos - %d%%\nVotos nulos - %d%%", eleitores, pValido, pBranco, pNulo);
+    } else {
+        printf("\nA quantidade de VOTOS VÁLIDOS ficou abaixo de 0, ou seja, as informações estão erradas, TENTE NOVAMENTE!");
     }
-
-    pBranco = (vBranco * 100) / eleitores;
-    pNulo =  (vNulo * 100) / eleitores;
-
-    printf("\nTotal de votos - %d\nVotos válidos - %d\nVotos brancos - %d\nVotos nulos - %d", eleitores, vValido, vBranco, vNulo);
 
     return 0;
 }
