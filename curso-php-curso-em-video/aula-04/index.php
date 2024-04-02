3b8094d15e03d1adad9031b9e3bcb558c9a6dbd9
Helder Neto <helderneto2005@gmail.com>
2024-04-02 03:33:35 -0300

    php curso

--- curso-php-curso-em-video/aula-04/index.php
+++ curso-php-curso-em-video/aula-04/index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
+<?php
+    $nome = "Helder";
+    $sobrenome = "Neto";
+    const PAIS = "Brasil";
     
-</body>
-
-</html>
+    echo "Olá, $nome $sobrenome! Vi que você mora no " . PAIS;
