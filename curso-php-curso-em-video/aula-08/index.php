a5274bb3445bccb8c074a256db2e2c0a840c3061
Helder Neto <helderneto2005@gmail.com>
2024-04-03 15:28:50 -0300

    curso php

--- curso-php-curso-em-video/aula-08/index.php
+++ curso-php-curso-em-video/aula-08/index.php
@@ -8,7 +8,38 @@
 </head>
 
 <body>
+    <?php
+        $res = abs(-2000);
+        echo "<p>$res</p>";
+        
+        $res = base_convert(254, 10, 2);
+        echo "<p>$res</p>";
 
+        $res = intdiv(5, 2);
+        echo "<p>$res</p>";
+
+        $res = min(5, 2, 0, 9, 7, 1);
+        echo "<p>$res</p>";
+        $res = max(5, 2, 0, 9, 7, 1);
+        echo "<p>$res</p>";
+        
+        $res = pi();
+        echo "<p>$res</p>";
+        $res = M_PI;
+        echo "<p>$res</p>";
+
+        $res = pow(5, 2);
+        echo "<p>$res</p>";
+        $res = 5 ** 2;
+        echo "<p>$res</p>";
+
+        $res = sqrt(9);
+        echo "<p>$res</p>";
+        $res = 9 ** (1/2);
+        echo "<p>$res</p>";
+        $res = 8 ** (1/3);
+        echo "<p>$res</p>";
+    ?>
 </body>
 
 </html>
