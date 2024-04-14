89bb366cd2d7d723f8af4690d7524f84d6cd1ad1
Helder Neto <helderneto2005@gmail.com>
2024-04-14 14:57:09 -0300

    Exercício da data finalizado

--- universidade-de-rio-verde/exercicio-012/q4.c
+++ universidade-de-rio-verde/exercicio-012/q4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <locale.h>
 
 int main(){
@@ -6,27 +7,34 @@ int main(){
 
     char data[10];
     int dia, mes, ano;
+    bool dataValida = false;
 
-    printf("Digite uma data no formato DD/MM/AAAA sem esquecer das /\nData: ");
+    printf("Digite uma data no formato DD/MM/AAAA sem esquecer das / (BARRAS)\nData: ");
     scanf("%s", data);
 
     sscanf(data, "%d/%d/%d", &dia, &mes, &ano);
 
-    if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 1900 || ano > 2100) {
-        printf("Mês inválido");
-    } else {
-        if (ano % 4 == 0) {
-            if (dia <= 29 && mes == 2 ) {
-
-            } else if (dia == 30 && (mes == 4 || mes == 6 || mes == 9 || mes == 11)) {
-
-            } else if (dia == 31 ) {
-
+    if ((dia >= 1 && dia <= 31) && (mes >= 1 && mes <= 12) && (ano >= 1900 && ano <= 2100)) {
+        if (ano % 4 == 0) { // BISSEXTO
+            if (dia <= 29) {
+                dataValida = true;
+            } else if (dia == 30 && mes != 2) {
+                dataValida = true;
+            } else if (dia == 31 && (mes != 2 && mes != 4 && mes != 6 && mes != 9 && mes != 11)) {
+                dataValida = true;
             }
         } else {
-
+            if (dia <= 28) {
+                dataValida = true;
+            } else if (dia <= 30 && mes != 2) {
+                dataValida = true;
+            } else if (dia == 31 && (mes != 2 && mes != 4 && mes != 6 && mes != 9 && mes != 11)) {
+                dataValida = true;
+            }
         }
     }
+
+    dataValida ? printf("\nData VÁLIDA") : printf("\nData INVÁLIDA");
 
     return 0;
 }
