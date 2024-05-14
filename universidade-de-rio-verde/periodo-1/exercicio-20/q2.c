3405edaa07d92d241aabf033a12c8b7cd5c21c08
Helder Neto <helderneto2005@gmail.com>
2024-05-14 17:40:30 -0300

    Fix atvd

--- universidade-de-rio-verde/periodo-1/exercicio-20/q2.c
+++ universidade-de-rio-verde/periodo-1/exercicio-20/q2.c
@@ -5,37 +5,15 @@ int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
 
     int tabuada, resultado;
-    char tabuadaTipo;
 
-    printf("(+) Adição\n(-) Subtração\n(*) Multiplicação\n(/) Divisão\nEscolha a tabuada de acordo com o símbolo que antescede ela: ");
-    scanf("%c", &tabuadaTipo);
+    do {
+        printf("Escolha uma tabuada de 1 - 10: ");
+        scanf("%d", &tabuada);
+    } while (tabuada < 1 || tabuada > 10);
 
-    printf("Escolha uma tabuada de 1 - 10: ");
-    scanf("%d", &tabuada);
-
-    for (int i = 0; i <= 10; i++) {
-        switch (tabuadaTipo) {
-            case '+':
-                resultado = tabuada + i;
-                break;
-            case '-':
-                resultado = tabuada - i;
-                break;
-            case '*':
-                resultado = tabuada * i;
-                break;
-            case '/':
-                resultado = (float) tabuada / i;
-                break;
-            default:
-                printf("Escolha um tipo de tabuada válida, as opção válidas são (+) (-) (*) (/): ");
-                while (tabuadaTipo != '+' && tabuadaTipo != '-' && tabuadaTipo != '*' && tabuadaTipo != '/') {
-                    scanf("%c", &tabuadaTipo);
-                }
-                break;
-
-        }
-        printf("%d %c %d = %d\n", tabuada, tabuadaTipo, i, resultado);
+    for (int i = 1; i <= 10; i++) {
+        resultado = tabuada * i;
+        printf("%d * %d = %d\n", tabuada, i, resultado);
     }
 
     return 0;
