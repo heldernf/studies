43a6eb0fb9aeb9754a7fe457aa724d40c55f3d85
Helder Neto <helderneto2005@gmail.com>
2024-05-22 23:11:36 -0300

    Create aula.lua

---
+++ curso-lua-guaxinim-scripts/exercicio-001/aula.lua
@@ -0,0 +1,15 @@
+nome = "Helder"
+print(nome)
+
+local idade = 18
+print(idade)
+
+coords = {x = 123, y = 321, z = 111}
+print(coords.y)
+
+function tst()
+    local five = "M"
+    print(five)
+end
+print(five)
+tst()
