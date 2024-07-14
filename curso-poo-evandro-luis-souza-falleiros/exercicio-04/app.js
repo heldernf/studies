974a77181ce8788fafa4bd8327e21ae13f1e7d9f
Helder Neto <helderneto2005@gmail.com>
2024-07-14 19:08:48 -0300

    script to app and template for atvd 4

---
+++ curso-poo-evandro-luis-souza-falleiros/exercicio-04/app.js
@@ -0,0 +1,10 @@
+import modificador from './modificador.js';
+
+var ingredientes = ["mel", "água", "sal", "mostarda"];
+
+var resultadoCapitalizado = modificador.capitalizar(ingredientes);
+var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
+var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
+console.log(resultadoCapitalizado)
+console.log(resultadoOrdenacao)
+console.log(resultadoCaixaAlta)
