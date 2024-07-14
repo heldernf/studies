7d2408459ad5eabe8c1fc82994963a80f5848511
Helder Neto <helderneto2005@gmail.com>
2024-07-14 19:19:52 -0300

    atvd 4

--- curso-poo-evandro-luis-souza-falleiros/exercicio-04/app.js
+++ curso-poo-evandro-luis-souza-falleiros/exercicio-04/app.js
@@ -1,10 +1,15 @@
 import modificador from './modificador.js';
+import ingredientes from './ingredientes.js';
 
-var ingredientes = ["mel", "água", "sal", "mostarda"];
+console.log(ingredientes)
+console.log(typeof(ingredientes))
+console.log(Array.isArray(ingredientes))
 
-var resultadoCapitalizado = modificador.capitalizar(ingredientes);
-var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
-var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
-console.log(resultadoCapitalizado)
-console.log(resultadoOrdenacao)
-console.log(resultadoCaixaAlta)
+// var ingredientes = ["mel", "água", "sal", "mostarda"];
+
+// var resultadoCapitalizado = modificador.capitalizar(ingredientes);
+// var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
+// var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
+// console.log(resultadoCapitalizado)
+// console.log(resultadoOrdenacao)
+// console.log(resultadoCaixaAlta)
