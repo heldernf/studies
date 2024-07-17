bfafa25fafa2ea04f90692dea9cad10d3cace3a9
Helder Neto <helderneto2005@gmail.com>
2024-07-17 14:57:36 -0300

    Update app.js

--- curso-poo-evandro-luis-souza-falleiros/exercicio-06/app.js
+++ curso-poo-evandro-luis-souza-falleiros/exercicio-06/app.js
@@ -2,12 +2,3 @@ import modificador from './modificador.js';
 import ingredientes from './ingredientes.js';
 
 modificador.capitalizarNovo(ingredientes, "nome")
-
-// var ingredientes = ["mel", "água", "sal", "mostarda"];
-
-// var resultadoCapitalizado = modificador.capitalizar(ingredientes);
-// var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
-// var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
-// console.log(resultadoCapitalizado)
-// console.log(resultadoOrdenacao)
-// console.log(resultadoCaixaAlta)
