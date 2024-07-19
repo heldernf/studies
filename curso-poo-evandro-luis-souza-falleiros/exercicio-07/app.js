73855b16ffbe29ec314e89bc7c43a1b67f749cb8
Helder Neto <helderneto2005@gmail.com>
2024-07-19 01:26:06 -0300

    atvd 7

--- curso-poo-evandro-luis-souza-falleiros/exercicio-07/app.js
+++ curso-poo-evandro-luis-souza-falleiros/exercicio-07/app.js
@@ -1,2 +1,15 @@
 import modificador from './modificador.js';
 import ingredientes from './ingredientes.js';
+
+let ingredientesOrdenados = modificador.ordenar(ingredientes, "nome");
+const containerIngredientes = document.getElementById("container-ingredientes");
+
+for (let ingrediente of ingredientesOrdenados) {
+    let textoHTML = `
+        <div class="ingrediente">
+            <img src="img/${ingrediente.img}" alt="Foto do ">
+            <p class="nome-ingrediente">${ingrediente.nome}</p>
+        </div>
+    `;1
+    containerIngredientes.innerHTML += textoHTML
+}
