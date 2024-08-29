5cb7c4f47ad9d833eef4cf4907a8fbc4c23af780
Helder Neto <helderneto2005@gmail.com>
2024-08-28 22:23:43 -0300

    atvd

--- universidade-de-rio-verde/periodo-2/exercicio-07/q3.c
+++ universidade-de-rio-verde/periodo-2/exercicio-07/q3.c
@@ -5,7 +5,17 @@
 int main() {
     setlocale(LC_ALL, "Portuguse_Brazil");
 
+    int vetor[6] = {1, 0, 5, -2, -5, 7};
     
-    
+    int soma = vetor[0] + vetor[1] + vetor[5];
+    printf("%d", soma);
+
+    vetor[4] = 100;
+
+    printf("\n\n");
+    for (int i = 0; i < 6; i++) {
+        printf("%d\n", vetor[i]);
+    }
+
     return 0;
 }
