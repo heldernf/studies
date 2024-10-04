66ddfda9b77e256ed87e8038afb9619a9faa4712
Helder Neto <helderneto2005@gmail.com>
2024-10-04 17:13:28 -0300

    atvd

---
+++ universidade-de-rio-verde/periodo-2/exercicio-12/q3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arrayLength = 10, array[arrayLength];
+
+    printf("Informe %d valores para que eu organize", arrayLength);
+    for (int i = 0; i < arrayLength; i++) {
+        printf("\nValor %d:", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    for (int i = 1; i < arrayLength; i++) {
+        for (int j = 0; j < arrayLength - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                int aux = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = aux;
+            }
+        }
+    }
+
+    for (int i = 0; i < arrayLength; i++) {
+        printf("%d ", array[i]);
+    }
+
+    return 0;
+}
