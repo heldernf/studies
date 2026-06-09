536a8d8597337e936079ac2eeb203682f82194f1
Helder Neto <helderneto2005@gmail.com>
2026-06-09 09:46:16 -0300

    hotfix(react/felipe_rocha): redireciona o user para example.com/home quando ele está em example.com/

--- courses/react/felipe_rocha/exercise_002/src/main.jsx
+++ courses/react/felipe_rocha/exercise_002/src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import TaskPage from './pages/TaskPage.jsx'
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to='/home' replace />
+  },
   {
     path: '/home',
     element: <App />,
