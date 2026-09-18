d25139f9ec34c0fc37830eb0173b1e6f9ae3f831
Helder Neto <helderneto2005@gmail.com>
2026-09-18 07:46:51 -0300

    feat(typescript/rincko-dev): add Import Alias exercise

---
+++ courses/typescript/rincko_dev/exercise_032/src/commands/public/functions/calculator.ts
@@ -0,0 +1,9 @@
+// import { sum } from '@/math/sum.js'
+// import { sub } from '@/math/sub.js'
+
+import { sum, sub } from '@/math'
+
+export function execute() {
+  console.log(sum(1, 2))
+  console.log(sub(1, 2))
+}
---
+++ courses/typescript/rincko_dev/exercise_032/src/index.ts
@@ -0,0 +1,8 @@
+// Import Alias
+
+import { execute } from '@/functions/calculator.js'
+
+import hey from '@/other/other.js'
+
+execute()
+hey()
